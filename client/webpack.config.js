1e3fba29682e2ab208f05fb15671a3fef97b739e
AliahG97 <143744575+AliahG97@users.noreply.github.com>
2024-04-13 18:26:42 -0400

    Add and configure workbox plugins for a service worker and manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,8 +4,6 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +16,43 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Webpack plugin that generates an html file and injects the bundles
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Text Editor",
+      }),
+
+      // Inject sw.js
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+
+      // Generate manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Text Editor",
+        short_name: "Text",
+        description: "Save text here!",
+        background_color: "#FFB6C1",
+        theme_color: "#FFB6C1" ,
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [95, 130, 190, 255, 385, 515],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
+    // TODO: Add CSS loaders and babel to webpack.
     module: {
       rules: [
-        
+
       ],
     },
   };
