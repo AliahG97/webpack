bb7013922bab52546c88fa531d15e77dfe7e2b42
AliahG97 <143744575+AliahG97@users.noreply.github.com>
2024-04-13 23:54:18 -0400

    Add an handler for the  event

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,7 +9,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
   butInstall.classList.toggle("hidden", false);
 });
 
-// TODO: Creates a click event handler on the `butInstall` 
+// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
     const promptEvent = window.deferredPrompt;
   
@@ -29,6 +29,7 @@ butInstall.addEventListener("click", async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
-    // Clear prompt
+    // Clears prompt
     window.deferredPrompt = null;
   });
+  
